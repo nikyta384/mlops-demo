d8232d13633f36aaf4af0c7c3304b0f618497101
nikyta384 <nikyta384@gmail.com>
2025-05-16 16:08:54 +0300

    bentofile.yaml edited 3

--- src/train_v2.py
+++ src/train_v2.py
@@ -29,7 +29,6 @@ def main():
     ])
 
     mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI", "http://127.0.0.1:5000"))
-    mlflow.set_experiment("tweet_sentiment_experiment")
 
     with mlflow.start_run() as run:
         pipeline.fit(X_train, y_train)
